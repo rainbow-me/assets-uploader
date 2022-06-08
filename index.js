5830fe229bcf144d7d02e72116646694dde18b69
osdnk <micosa97@gmail.com>
2022-06-08 14:39:58 +0200

    init

---
+++ index.js
@@ -0,0 +1,64 @@
+const cloudinary = require('cloudinary').v2
+const yargs = require('yargs/yargs')
+const fs = require("fs")
+const fetch = require("node-fetch")
+const { hideBin } = require('yargs/helpers')
+
+const link  = (code, chain) => `https://rainbowme-res.cloudinary.com/image/upload/assets/${chain}/${code}.png`
+
+const argv = yargs(hideBin(process.argv)).argv
+
+const apiKey = argv.key
+const apiSecret = argv.secret
+const forceUpload = argv.force
+
+cloudinary.config({
+  cloud_name: 'rainbowme',
+  api_key: apiKey,
+  api_secret: apiSecret
+});
+
+const chains = ["arbitrum", "polygon", "optimism", "ethereum"]
+
+
+const blockchainsFolder = argv.dir + "/blockchains/"
+
+const logos = []
+for (let chain of chains) {
+    const assetCodes = fs.readdirSync(blockchainsFolder  + chain + "/assets");
+    for (let code of assetCodes) {
+        const logo = blockchainsFolder + chain + "/assets/" + code + "/logo.png"        
+        logos.push([chain, code, logo])
+    }
+}
+
+async function uploadImages() {
+    let current = 0;
+    const total = logos.length
+    let startTime = Date.now()
+    for (let [chain, code, logo] of logos) {
+        console.log(`Uploading... ${chain}/${code} (${current++}/${total})`)
+        if (current % 10 === 0) {
+            const remainingMs = ((Date.now() - startTime) * (total - current) / current)
+            const remainingMinutes = remainingMs / 1000 / 60
+            console.log("Left in munites: " + remainingMinutes)
+        }
+        try {
+            const cloudinaryLink = link(code, chain)
+            if (!forceUpload) {
+                const res = await fetch(cloudinaryLink)
+                if (res.status === 200) {
+                    console.log("Skipped.")
+                    continue;
+                }
+            }
+            const res = await cloudinary.uploader.upload(logo, { public_id: "assets/" + chain + "/" + code })
+            console.log("Done.")
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+}
+
+uploadImages()
