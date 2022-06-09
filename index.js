a7750f97a17c21f8b6afabaf3e2028701c74f703
osdnk <micosa97@gmail.com>
2022-06-09 11:10:25 +0300

    add commit chekcing

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const yargs = require('yargs/yargs')
 const fs = require("fs")
 const fetch = require("node-fetch")
 const { hideBin } = require('yargs/helpers')
+const { spawnSync, exec, execSync } = require('child_process');
+
+// console.error('error', child.error);
+// console.log('stdout ', child.stdout);
+// console.error('stderr ', child.stderr);
 
 const link  = (code, chain) => `https://rainbowme-res.cloudinary.com/image/upload/assets/${chain}/${code}.png`
 
@@ -23,12 +28,26 @@ const chains = ["arbitrum", "polygon", "optimism", "ethereum"]
 
 const blockchainsFolder = argv.dir + "/blockchains/"
 
+const recentCommits = JSON.parse(fs.readFileSync("./lastCommits.json", "utf-8"))
+
+let i = 0
 const logos = []
+
+const commitHash = execSync(`git -C ./../assets log -n 1 --pretty=format:%H -- blockchains/ethereum/assets/0x0cd022dde27169b20895e0e2b2b8a33b25e63579/logo.png | cat`, { encoding : 'utf8' })
+
+console.log(commitHash === "", undefined)
 for (let chain of chains) {
-    const assetCodes = fs.readdirSync(blockchainsFolder  + chain + "/assets");
+    const assetCodes = fs.readdirSync(blockchainsFolder + chain + "/assets");
     for (let code of assetCodes) {
-        const logo = blockchainsFolder + chain + "/assets/" + code + "/logo.png"        
-        logos.push([chain, code, logo])
+        console.log(`Analysing... ${chain}/${code}`)
+        const logo = blockchainsFolder + chain + "/assets/" + code + "/logo.png"
+        const commitHash = execSync(`git -C ${argv.dir} log -n 1 --pretty=format:%H -- blockchains/${chain}/assets/${code}/logo.png | cat`, { encoding : 'utf8' })
+        if (commitHash !== "" && commitHash !== recentCommits[code.toLowerCase()]) {
+            recentCommits[code.toLowerCase()] = commitHash.toLowerCase()
+            logos.push([chain, code, logo])
+        } else {
+            console.log("Skipped.")
+        }
     }
 }
 
@@ -36,6 +55,7 @@ async function uploadImages() {
     let current = 0;
     const total = logos.length
     let startTime = Date.now()
+    console.log(`--------- UPLOADING ${total} asset(s). ---------`)
     for (let [chain, code, logo] of logos) {
         console.log(`Uploading... ${chain}/${code} (${current++}/${total})`)
         if (current % 10 === 0) {
@@ -44,14 +64,14 @@ async function uploadImages() {
             console.log("Left in munites: " + remainingMinutes)
         }
         try {
-            const cloudinaryLink = link(code, chain)
-            if (!forceUpload) {
-                const res = await fetch(cloudinaryLink)
-                if (res.status === 200) {
-                    console.log("Skipped.")
-                    continue;
-                }
-            }
+            // const cloudinaryLink = link(code, chain)
+            // if (!forceUpload) {
+            //     const res = await fetch(cloudinaryLink)
+            //     if (res.status === 200) {
+            //         console.log("Skipped.")
+            //         continue;
+            //     }
+            // }
             const res = await cloudinary.uploader.upload(logo, { public_id: "assets/" + chain + "/" + code })
             console.log("Done.")
         } catch (e) {
